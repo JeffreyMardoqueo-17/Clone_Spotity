224a1a404ef2cec30dbe9699de7ed673260c88c8
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2024-10-17 19:41:50 -0600

    Aqui ya detecto el audio, pero no tiene diseño

--- src/components/AudioPlayer.tsx
+++ src/components/AudioPlayer.tsx
@@ -1,9 +1,15 @@
-import React from 'react'
+"use client";
+
+import React from 'react';
+import ReactAudioPlayer from 'react-audio-player';
 
 export default function AudioPlayer() {
     return (
         <div>
-            
+            <ReactAudioPlayer
+                src="/TheNigthWeMeet.mp3"  // Ruta directa al archivo dentro de public
+                controls
+            />
         </div>
-    )
+    );
 }
